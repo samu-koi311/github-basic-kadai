0c1c07ff9c5ab4199d2edb469a22f5a46969de88
samu-koi311 <167400621+samu-koi311@users.noreply.github.com>
2024-04-22 10:35:37 +0900

    kadai_018を提出

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,27 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+   //姓フィールド
+	public String familyName = "加藤";
+	//名フィールド
+	public String givenName;
+	//住所フィールド
+	public String address = "東京都中野区〇×";
+	
+	//共通の紹介を出力するメソッド
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName + givenName + "です");
+		System.out.println("住所は" + address + "です");
+	
+	}
+	//個別の紹介を出力するメソッド
+	abstract void eachIntroduce();
+	
+	
+	
+	//紹介を実行するメソッド
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
