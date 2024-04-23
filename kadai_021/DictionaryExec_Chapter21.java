5cea9a1224e3a91aaf3e85e2995d0cbdb65907db
samu-koi311 <167400621+samu-koi311@users.noreply.github.com>
2024-04-23 11:41:48 +0900

    課題２１をアップロード

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,14 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+	Dictionary_Chapter21 fruits = new Dictionary_Chapter21();
+	
+	String word[] = {"apple","banana","grape","orange"};
+	
+	fruits.Dictionary(word);
+
+
+	}
+
+}
